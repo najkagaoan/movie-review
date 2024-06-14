dad6f3da5a8ded1bee5766b17dbedb9f302b003a
Naj Kagaoan <nkagaoan@atlassian.com>
2024-06-14 12:36:45 +1000

    movie api backend

--- src/main/java/com/example/movies/MoviesApplication.java
+++ src/main/java/com/example/movies/MoviesApplication.java
@@ -2,20 +2,11 @@ package com.example.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@SpringBootApplication
-@RestController
+@SpringBootApplication()
 public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
-	}
-
-	@GetMapping
-	public String root(){
-		return "Hello, World!";
 	}
 
 }
