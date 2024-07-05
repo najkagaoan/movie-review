ca61ae251dc526e4a9a1af1e57f4920d588e7b8f
Naj Kagaoan <nkagaoan@atlassian.com>
2024-07-06 09:20:54 +1000

    cross origin changes

--- src/main/java/com/example/movies/movie/MovieController.java
+++ src/main/java/com/example/movies/movie/MovieController.java
@@ -7,12 +7,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/movies")
+@CrossOrigin(origins = "*")
 public class MovieController {
 
     @Autowired
--- src/main/java/com/example/movies/review/ReviewController.java
+++ src/main/java/com/example/movies/review/ReviewController.java
@@ -11,6 +11,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/reviews")
+@CrossOrigin(origins = "*")
 public class ReviewController {
 
     @Autowired
